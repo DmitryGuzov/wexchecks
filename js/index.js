1332bb1168fef72abafb93418fe29443ced01acd
Dmytro Huzov <49764703+RaiSky@users.noreply.github.com>
2020-12-29 22:08:30 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,36 @@
+$( () => {
+	
+	//On Scroll Functionality
+	$(window).scroll( () => {
+		var windowTop = $(window).scrollTop();
+		windowTop > 100 ? $('nav').addClass('navShadow') : $('nav').removeClass('navShadow');
+		windowTop > 100 ? $('ul.menu-list').css('top','60px') : $('ul.menu-list').css('top','100px');
+	});
+	
+	//Click Logo To Scroll To Top
+	$('#logo').on('click', () => {
+		$('html,body').animate({
+			scrollTop: 0
+		},500);
+	});
+	
+	//Smooth Scrolling Using Navigation Menu
+	$('a[href*="#"]').on('click', function(e){
+		$('html,body').animate({
+			scrollTop: $($(this).attr('href')).offset().top - 60
+		},500);
+		e.preventDefault();
+	});
+	
+	//Toggle Menu
+	$('#menu-toggle').on('click', () => {
+		$('#menu-toggle').toggleClass('closeMenu');
+		$('ul').toggleClass('showMenu');
+		
+		$('li').on('click', () => {
+			$('ul').removeClass('showMenu');
+			$('#menu-toggle').removeClass('closeMenu');
+		});
+	});
+	
+});
